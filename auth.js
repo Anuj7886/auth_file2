73874b4b4fbc216e33e1c92aa6a52b699c1c1ddf
Anuj7886 <anuj6662004@gmail.com>
2024-10-05 12:41:33 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const router = express.Router();
+
+// Auth routes
+router.post('/login', (req, res) => {
+  // Login logic
+});
+
+router.post('/register', (req, res) => {
+  // Register logic
+});
+
+export default router;
